554821991e9a9a124f2bc16315f5d72b662365a1
oneilld136 <oneilld136@gmail.com>
2019-04-24 11:35:50 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name
+  end
+
+  def initialize(job)
+    @job = job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+
+# beyonce = Person.new
+# beyonce.name= "Beyonce"
+# puts beyonce.name
